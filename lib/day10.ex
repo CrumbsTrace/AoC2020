f48af66b58706a9729f2b7e359944abac55974fa
CrumbsTrace <v.a.kalkman@gmail.com>
2020-12-11 00:31:15 +0100

    even more cleanup

--- lib/day10.ex
+++ lib/day10.ex
@@ -19,23 +19,19 @@ defmodule Day10 do
   def p1([fst, snd |tail], {jolt1, jolt3}) when snd - fst == 1, do: p1([snd | tail], {jolt1 + 1, jolt3    })
   def p1([fst, snd |tail], {jolt1, jolt3}) when snd - fst == 3, do: p1([snd | tail], {jolt1    , jolt3 + 1})
 
-  def p2([_                    ], seen), do: {1, seen}
-  def p2([fst, snd       | []  ], seen), do: if(snd - fst == 3, do: {1, seen} , else: {0, seen})
-  def p2([fst, snd, thrd | tail], seen) do
-    case Map.get(seen, fst) do
-      nil       -> {fst, 0, seen, [snd, thrd | tail]} |> check_next_adapter() |> check_next_adapter() |> check_next_adapter() |> collect_result()
-      value -> {value, seen}
-    end
-  end
+  def p2([_              ], seen), do: {1, seen}
+  def p2([fst, snd | []  ], seen), do: if(snd - fst == 3,             do: {1, seen}    , else: {0, seen})
+  def p2([fst      | tail], seen), do: if(value = Map.get(seen, fst), do: {value, seen}, else: go_through_adapter_options(fst, tail, seen))
 
-  def check_next_adapter({_ , _    , seen, [        ]}),                                 do: {0 , 1    , seen, []  }
-  def check_next_adapter({v1, count, seen, [v2 |tail]}) when not (v2 - v1 in [1, 2, 3]), do: {v1, count, seen, tail}
-  def check_next_adapter({v1, count, seen, [v2 |tail]}) do
-    {new_count, new_seen} = p2([v2|tail], seen)
-    {v1, count + new_count, new_seen, tail}
-  end
+  def go_through_adapter_options(fst, tail, seen), do: {0, seen, fst, tail} |> try_next_adapter |> try_next_adapter |> try_next_adapter |> collect_result
 
-  def collect_result({v1, count, seen, _}), do: {count, Map.put(seen, v1, count)}
+  def try_next_adapter({_    , seen, v1, [        ]}),                                 do: {1    , seen, v1, []  }
+  def try_next_adapter({count, seen, v1, [v2 |tail]}) when not (v2 - v1 in [1, 2, 3]), do: {count, seen, v1, tail}
+  def try_next_adapter({count, seen, v1, [v2 |tail]}),                                 do: p2([v2 | tail], seen) |> process_p2_result(count, v1, tail)
+
+  def process_p2_result({branch_count, new_seen}, count, v1, tail), do: {count + branch_count, new_seen, v1, tail}
+
+  def collect_result({count, seen, v1, _}), do: {count, Map.put(seen, v1, count)}
 
   def parse_input(input), do: input |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1) |> (& [0, Enum.max(&1) + 3 | &1]).() |> Enum.sort()
 
